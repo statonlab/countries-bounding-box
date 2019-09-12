3e8abb70ed55ecc176ee8e9f33624d67c6cf97fa
Abdullah Almsaeed <almasaeed2010@gmail.com>
2019-09-12 09:09:28 -0400

    Extract correct bounds

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import json
+from geopy import distance
 
 
 def makeCoordinates(coords):
-    if len(coords) % 2 != 0:
-        raise Exception('Invalid coordinates {}', coords)
-
-    i = 0
     result = []
-
+    i = 0
     while i < len(coords):
-        lat = coords[i]
-        lng = coords[i+1]
-        i += 2
+        if len(coords[i]) % 2 != 0:
+            raise Exception('Invalid coordinates {}', coords)
+
+        lat = coords[i][0]
+        lng = coords[i][1]
         result.append((lat, lng))
+        i += 2
 
     return result
 
@@ -20,8 +20,13 @@ def makeCoordinates(coords):
 def flatten(stack):
     result = []
     while(len(stack) > 0):
+        if isinstance(stack[0][0], float) or isinstance(stack[0][0], int):
+            result += makeCoordinates(stack)
+            stack.pop(0)
+            continue
+
         item = stack[0]
-        if isinstance(item[0], float) or isinstance(item[0], int):
+        if isinstance(item[0][0], float) or isinstance(item[0][0], int):
             result += makeCoordinates(item)
             stack.pop(0)
         else:
@@ -29,19 +34,33 @@ def flatten(stack):
             for i in item:
                 flat += i
             stack[0] = flat
+
     return result
 
 
 def bounds(coordinates):
     # find min/max
+    lat = float(coordinates[0][1])
+    lng = float(coordinates[0][0])
+
     minLat = 180
     minLng = 90
     maxLat = -180
     maxLng = -90
 
     for coord in coordinates:
-        lat = float(coord[0])
-        lng = float(coord[1])
+        lat = float(coord[1])
+        lng = float(coord[0])
+
+        if lat > 90:
+            lat = 90
+        if lat < -90:
+            lat = -90
+        if lng > 180:
+            lng = 180
+        if lng < -180:
+            lng = -180
+
         minLat = min(minLat, lat)
         minLng = min(minLng, lng)
         maxLat = max(maxLat, lat)
@@ -49,12 +68,12 @@ def bounds(coordinates):
 
     return {
         'southWest': {
-            'lat': round(minLat, 6),
-            'lng': round(minLng, 6)
+            'lat': minLat,
+            'lng': minLng
         },
         'northEast': {
-            'lat': round(maxLat, 6),
-            'lng': round(maxLng, 6)
+            'lat': maxLat,
+            'lng': maxLng
         }
     }
 
@@ -62,14 +81,16 @@ def bounds(coordinates):
 with open('./countries.geojson') as f:
     data = json.load(f)
 
-
 countries = []
 for feature in data['features']:
     props = feature['properties']
+
+    boxes = bounds(flatten(feature['geometry']['coordinates']))
+
     countries.append({
         'name': props['ADMIN'],
         'iso': props['ISO_A3'],
-        'bounds': bounds(flatten(feature['geometry']['coordinates']))
+        'bounds': boxes
     })
 
 
